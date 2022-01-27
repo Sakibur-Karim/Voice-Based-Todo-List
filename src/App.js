5320c08a362ba4b22063578bf9dd501d861b2c88
Sakib <sakiburkarim@gmail.com>
2022-01-26 22:50:11 -0500

    Updated

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
 
   return (
     <>
-      <Todo />
+      <Todo databaseRef={databaseRef}/>
     </>
   )
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,25 +1,35 @@
 import React from 'react'
 import { FiX } from 'react-icons/fi'
-export default function Todo() {
+import { useEffect } from 'react'
+import { getDocs } from 'firebase/firestore'
+import { useState } from 'react'
+
+
+export default function Todo({ databaseRef }) {
+  const [todoList, setTodoList] = useState([])
+
+  useEffect(() => {
+    const getData = async () => {
+      let data = await getDocs(databaseRef);
+      setTodoList(data.docs.map((item) => ({ ...item.data(), id: item.id })));
+    }
+    getData()
+  }, [])
+
+
   return (
     <>
       <div className='todo-main'>
         <h1 className='header'>Todo React</h1>
         <div className='todo-card'>
-          <div className='todo-list'>
-            <h2>Play</h2>
-            <FiX className='close-icon' />
-          </div>
-
-          <div className='todo-list'>
-            <h2>Kill</h2>
-            <FiX className='close-icon' />
-          </div>
-
-          <div className='todo-list'>
-            <h2>Destroy</h2>
-            <FiX className='close-icon' />
-          </div>
+          {todoList.map((todo) => {
+            return (
+              <div className='todo-list'>
+                <h3>{todo.item}</h3>
+                <FiX className='close-icon' />
+              </div>
+            )
+          })}
         </div>
       </div>
     </>
